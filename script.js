97af6457c5624aa46a6cb62dd5b99f9fa1437425
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-04-10 11:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function toCase(text) {
   // write your code here
+	if (text.length === 0) {
+    return '-';
+  }
+
+  const lowerCaseText = text.toLowerCase();
+  const upperCaseText = text.toUpperCase();
+
+  return lowerCaseText + '-' + upperCaseText;
 }
 
 // DO not change the code below
